1376848134ea6cf18dd8d941aca90c66e8e237e5
ANN YIMLA-OR <ann.yiml@bulive.net>
2016-02-10 12:46:59 +0700

    jump ball

---
+++ game jam/Assets/SphereMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMove : MonoBehaviour 
+{
+	public float speed;
+	private Rigidbody rb;
+
+	void start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Fixedupdate() 
+	{
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			rb.AddForce(Vector3.up * speed);
+		}
+		                  
+	}
+
+}
+
